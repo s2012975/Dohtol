f7e75708077f076376b1e851d2bd09dd02888396
EC2 Default User <ec2-user@ip-172-31-40-170.ap-northeast-1.compute.internal>
2020-12-25 14:11:42 +0000

    [add]deviseのcustomersとadmins

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  devise_for :admins, controllers: {
+    sessions:      'admins/sessions',
+    passwords:     'admins/passwords',
+    registrations: 'admins/registrations'
+  }
+    # controllers以下を記入しないとviewに変更が加えられない。→ルートがきちんと通らない
+  devise_for :customers, controllers: {
+    sessions:      'customers/sessions',
+    passwords:     'customers/passwords',
+    registrations: 'customers/registrations'
+  }
+  
 end
