32c3bfec4ac51f46a2faaa14ce777db91f411a19
EC2 Default User <ec2-user@ip-172-31-40-170.ap-northeast-1.compute.internal>
2021-01-09 05:32:30 +0000

    [add]非同期通信コメント

---
+++ app/controllers/customers/likes_controller.rb
@@ -0,0 +1,2 @@
+class Customers::LikesController < ApplicationController
+end
--- app/models/story.rb
+++ app/models/story.rb
@@ -1,6 +1,6 @@
 class Story < ApplicationRecord
 
-  has_many :rates
+  has_many :likes
   belongs_to :customer
   belongs_to :qualification, dependent: :destroy
 
