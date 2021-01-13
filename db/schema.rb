7fdee52bba9cbb6bcf693680e912dc783c1a308b
EC2 Default User <ec2-user@ip-172-31-40-170.ap-northeast-1.compute.internal>
2021-01-13 09:48:16 +0000

    [update]レイアウト

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_09_042725) do
+ActiveRecord::Schema.define(version: 2021_01_13_091730) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -83,7 +83,7 @@ ActiveRecord::Schema.define(version: 2021_01_09_042725) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "is_active"
+    t.boolean "is_active", default: true
   end
 
   create_table "likes", force: :cascade do |t|
